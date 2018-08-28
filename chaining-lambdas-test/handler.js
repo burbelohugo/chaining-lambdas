d9573a4448b1aabacfd611bc2484bbb82b1a3fb5
Hugo Burbelo <hugoburbelo@gmail.com>
2018-08-28 14:58:49 -0400

    Add double lambda boilerplate

--- chaining-lambdas-test/handler.js
+++ chaining-lambdas-test/handler.js
@@ -1,14 +1,25 @@
-'use strict';
+const AWS = require('aws-sdk')
 
-module.exports.hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.test = async (event, context) => {
+  let lambda = new AWS.Lambda()
+  let params = {
+    FunctionName: 'chaining-lambda-dev-test2'
+  }
+  // lambda.invoke(params, function (err, data){
+  //   if (err) {
+  //     console.log('error : ' + err)
+  //     callback(err, null)
+  //   } else if (data) {
+  //     const response = {
+  //       statusCode: 200,
+  //       body: JSON.parse(data.Payload)
+  //     }
+  //     callback(null, response)
+  //   }
+  // })
+  return (null, {message: 'testing functionality'})
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.test2 = async (event, context) => {
+  return (null, {message: 'testing functionality 2'})
+}
