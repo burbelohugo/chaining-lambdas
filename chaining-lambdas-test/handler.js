c51b344d3131d3810032e1beff409154e682af69
Hugo Burbelo <hugoburbelo@gmail.com>
2018-09-03 14:53:58 -0400

    Add SNS chain

--- chaining-lambdas-test/handler.js
+++ chaining-lambdas-test/handler.js
@@ -1,23 +1,24 @@
 const AWS = require('aws-sdk')
 
 module.exports.hello = (event, context, callback) => {
-  var lambda = new AWS.Lambda()
-  var opts = {
-    FunctionName: 'chaining-lambdas-test-dev-ciao'
-  }
 
-  lambda.invoke(opts, function (err, data) {
-    if (err) {
-      console.log('error : ' + err)
-      callback(err, null)
-    } else if (data) {
-      const response = {
-        statusCode: 200,
-        body: JSON.parse(data.Payload)
-      }
-      callback(null, response)
-    }
-  })
+var AWS = require('aws-sdk');
+AWS.config.region = 'us-east-1';
+    var sns = new AWS.SNS();
+    console.log('arn:aws:sns:us-east-1:382494799516:ciao-dev')
+    sns.publish({
+        Message: 'Test publish to SNS from Lambda',
+        TopicArn: 'arn:aws:sns:us-east-1:382494799516:ciao-dev'
+    }, function(err, data) {
+        if (err) {
+            console.log(err.stack);
+            return;
+        }
+        console.log('push sent');
+        console.log(data);
+        callback(null, { message: 'yay!' })
+    });
+
 }
 
 module.exports.ciao = (event, context, callback) => {
